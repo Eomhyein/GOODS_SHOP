fe11fa18829ae2fe577b8ab184f4d3bea4a11bad
hyein <mickey_8688@naver.com>
2024-06-25 10:48:06 +0900

    #3 : create api_01

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,49 +1,33 @@
 // routes/products.router.js
 import express from 'express';
-// mongoose
-import mongoose from 'mongoose';
 import products from '../schemas/product.schema.js'; // 상품 관리하는 스키마
 
 // express.js의 라우터를 생성합니다.
-const router = express.Router();
+const productsRouter = express.Router();
 
 // 상품 등록 API구현
-router.post('/', async (req, res, next) => {
+productsRouter.post('/products', async (req, res, next) => {
   try {
     // 클라이언트로 부터 전달받은 데이터를 가져온다
-    // id, name, description, manager, status, createdAt, updateAt
-    const { name, description, password, manager, status } = req.body;
-    // name값이 중복되지 않는지 검사 => 실제로 몽고디비에 데이터를 조회해서, 해당하는 데이터가 몽고디비에 존재하는지 확인한다.
-    const existingProducts = await products.find({name}).exec(); 
-      
-    // 만약 중복되면 에러메시지 전달
-    // status 400: message 상품 정보를 모두 입력해 주세요.
-    // 400: 이미 등록 된 상품입니다.
-    if(existingProducts.length) {
-      return res.status(400).json({status:400, message: '이미 등록된 상품입니다.'});
-    }
-  
-// products 상품 생성
-// const createdProducts = await products.create ({
-//   name, 
-//   description, 
-//   password,
-//   manager, 
-//   status, 
-//   createdAt, 
-//   updateAt,
-// });
-  
-// // 생성된 상품 정보를 클라이언트에게 응답(Response) 반환한다.
-// // 201: 상품 생성에 성공했습니다.
-// return res.status(201).json({status:201, message: '상품 생성에 성공했습니다.', createdProduct});
+    // name, description, manager, password를 body로 가져온다.
+    const { name, description, manager, password } = req.body;
+
+    // DB에 저장하기
+    const product = new products({ name, description, manager, password });
+    const createdProduct = await product.save();
+    
+    // 생성된 상품 정보를 클라이언트에게 응답(Response) 반환한다.
+    // 201: 상품 생성에 성공했습니다.
+    return res
+      .status(201)
+      .json({status:201, message: '상품 생성에 성공했습니다.', createdProduct});
   } catch (error) {
     next(error);
   }
-
+});
 
 // 상품 목록 조회
-router.get('/', async (req, res) => {
+productsRouter.get('/products', async (req, res) => {
   // 상품 목록 데이터 가져오기
   // const createdProducts = await products.find().sort('-order').exec();
   // // 200 : 상품 목록 조회에 성공했습니다.
@@ -60,5 +44,5 @@ router.get('/', async (req, res) => {
 
 // 상품 삭제
 // 200: 상품 삭제에 성공했습니다.
-});
-export default router;
+
+export default productsRouter;
--- src/schemas/product.schema.js
+++ src/schemas/product.schema.js
@@ -13,17 +13,21 @@
 
 // mongoose 가져오기
 import mongoose from "mongoose";
+
+// PRODUCT_STATUS 상수 정의
+const PRODUCT_STATUS = {
+  FOR_SALE: "FOR_SALE",
+  SOLD_OUT: "SOLD_OUT",
+};
+
 // 스키마 작성
 const productsSchemas = new mongoose.Schema({
   name : {
     type: String,
     required : true,
+    unique: true,
   },
   description : {
-    type: String,
-    required : true,
-  },
-  password : {
     type: String,
     required : true,
   },
@@ -31,19 +35,19 @@ const productsSchemas = new mongoose.Schema({
     type: String,
     required : true,
   },
+  password : {
+    type: String,
+    required : true,
+    select: false,
+  },
   status : {
     type: String,
-    required : false,
-  },
-  createdAt : {
-    type: Date,
-    required : false,
-  },
-  updatedAt : {
-    type: Date,
-    required : false,
-  },
-})
+    required : true,
+    enum: Object.values(PRODUCT_STATUS),
+    default: PRODUCT_STATUS.FOR_SALE, // 기본값은 FOR_SALE
+  },},
+  { timestamps: true, toJSON: { virtuals : true }},
+)
 // 스키마 통해 모델 구현
 // 모델 외부로 보내기
 export default mongoose.model('Product', productsSchemas);
